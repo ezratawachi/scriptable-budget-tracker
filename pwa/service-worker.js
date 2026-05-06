fa699d88a91ead84cbd505ec50579ced1df3941e
ezratawachi <142938760+ezratawachi@users.noreply.github.com>
2026-05-06 18:08:12 -0500

    Add polished PWA icon and launch screens

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-pwa-v3"
+const CACHE_NAME = "budget-pwa-v4"
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,8 +6,20 @@ const ASSETS = [
   "./app.js",
   "./vendor/supabase.min.js",
   "./manifest.webmanifest",
+  "./icons/icon-180.png",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "./icons/icon-1024.png",
+  "./splash/launch-640x1136.png",
+  "./splash/launch-750x1334.png",
+  "./splash/launch-828x1792.png",
+  "./splash/launch-1125x2436.png",
+  "./splash/launch-1170x2532.png",
+  "./splash/launch-1179x2556.png",
+  "./splash/launch-1242x2208.png",
+  "./splash/launch-1242x2688.png",
+  "./splash/launch-1284x2778.png",
+  "./splash/launch-1290x2796.png"
 ]
 
 self.addEventListener("install", event => {
